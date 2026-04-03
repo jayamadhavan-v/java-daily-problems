1f09397e7cc6c87bfaad313098063366e72f9a49
jayamadhavan-v <jayamadhavan.vjm@gamil.com>
2026-04-03 13:01:24 +0530

    initial setup and demo code

--- java-codes/src/Demo.java
+++ java-codes/src/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("this demo code to check the inteliij and git hub connection");
+        System.out.println("bjbvbbv");
     }
 }
