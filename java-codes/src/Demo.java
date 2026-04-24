6ed1d2de275a7f9a25d8a747fedaa299939519ee
jayamadhavan-v <jayamadhavan.vjm@gamil.com>
2026-04-24 21:03:22 +0530

    day 1

--- java-codes/src/Demo.java
+++
@@ -1,6 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("this demo code to check the inteliij and git hub connection");
-        System.out.println("bjbvbbv");
-    }
-}
---
+++ java-codes/src/problems/basic/Odd_or_Even.java
@@ -0,0 +1,13 @@
+package problems.basic;
+
+import java.util.*;
+
+public class Odd_or_Even {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        System.out.println( n%2 == 0 ? "Even" : "Odd");
+    }
+}
+
+
